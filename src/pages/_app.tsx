980c1b152c242889d1c7a8f560a957c6d514369d
Merdan Turan <merdanturan@gmail.com>
2024-05-12 18:15:24 +0300

    fix load issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,9 +8,9 @@ import Header from "@/components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  useEffect(() => {
-    require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  }, []);
+  // useEffect(() => {
+  //   require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  // }, []);
   
   return (
     <Provider store={store}>
