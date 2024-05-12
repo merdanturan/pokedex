99768f99d730af174e7c3bf7dc1e54c066b9475f
Merdan Turan <merdanturan@gmail.com>
2024-05-12 18:24:14 +0300

    fix css issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css"
+import "../styles/globals.css"
 import "bootstrap/dist/css/bootstrap.min.css";
 import type { AppProps } from "next/app"
 import { Provider } from 'react-redux';
@@ -8,9 +8,9 @@ import Header from "@/components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
 
-  // useEffect(() => {
-  //   require("bootstrap/dist/js/bootstrap.bundle.min.js");
-  // }, []);
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   
   return (
     <Provider store={store}>
